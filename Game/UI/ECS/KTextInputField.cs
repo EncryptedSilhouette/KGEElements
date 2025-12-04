751f84401e52819d5875aedaf404814628109023
KSilhouette <83173321+EncryptedSilhouette@users.noreply.github.com>
2025-12-04 10:35:32 -0700

    update

--- Game/UI/ECS/KTextInputField.cs
+++ Game/UI/ECS/KTextInputField.cs
@@ -1,37 +1,9 @@
-﻿using Elements.Core;
-using Elements.Rendering;
-using System.Text;
-
-namespace Elements.Game.UI.ECS
+﻿namespace Elements.Game.UI.ECS
 {
     public class KTextInputField
     {
-        private bool _focused = false;
-        private StringBuilder _inputBuffer = new();
-
+        public KText TextBox;
         public KButton Button;
 
-        public KTextInputField(int x, int y, int width, int height, string text)
-        {
-            Button = new KButton(x, y, width, height, text);
-        }
-
-        public void Update(in float mPosX, in float mPosY)
-        {
-            if (_focused)
-            {
-                if (KProgram.InputManager.TextBufferLength > 0) 
-                {
-                    _inputBuffer.Append(KProgram.InputManager.ReadTextBuffer());
-                }
-            }
-            Button.Update(mPosX, mPosY);
-        }
-
-        public void FrameUpdate(KRenderManager renderManager)
-        {
-            
-            //Button.FrameUpdate(mPosX, mPosY);
-        }
     }
 }
